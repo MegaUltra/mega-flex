947b36b64913c90b50cd8806d21f2645f24dd2ac
Mikel-Jon West <dwmyke@gmail.com>
2020-03-15 09:30:59 -0700

    Image adjustments, can't figure out how to shadow hero block

--- src/gatsby-theme-flex/theme.js
+++ src/gatsby-theme-flex/theme.js
@@ -2,6 +2,13 @@
 // See https://theme-ui.com/theme-spec.
 export default {
   colors: {
-    primary: `#ff6b6b`,
+    text: "#0D0D0D",
+      background: "#E8E8E8",
+      primary: "#F28705",
+      secondary: "#BF369B",
+      muted: '#F2E9F2',
+      highlight: '#0C46F2',
+      gray: '#777',
+      accent: '#F2EAC2',
   },
 }
